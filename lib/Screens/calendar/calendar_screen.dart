630ade9f0fcf16a7c60e3f731bbb4bd59fe7b1d2
i-kye <grados0602@gmail.com>
2025-05-27 15:08:26 +0900

    feat: Update custom button components with styling

--- lib/Screens/calendar/calendar_screen.dart
+++ lib/Screens/calendar/calendar_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:personalized_travel_recommendations/theme/app_colors.dart';
+import 'package:personalized_travel_recommendations/screens/calendar/add_travel_plan_continent_screen.dart';
+import 'package:personalized_travel_recommendations/theme/app_text_styles.dart';
+import 'package:personalized_travel_recommendations/widget/custom_button.dart';
 
 class TravelCalendarScreen extends StatelessWidget {
   const TravelCalendarScreen({super.key});
@@ -9,8 +12,25 @@ class TravelCalendarScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.white,
       appBar: AppBar(
-        title: const Text('나의 여행 캘린더'),
+        title: const Text(
+          '나의 여행 캘린더',
+          style: AppTypography.subtitle20Bold,
+        ),
         backgroundColor: AppColors.white,
+      ),
+      body: Center(
+        child: CustomButton(
+          text: '여행 계획 추가',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddTravelPlanContinentScreen(),
+              ),
+            );
+          },
+          color: AppColors.indigo60,
+        ),
       ),
     );
   }
