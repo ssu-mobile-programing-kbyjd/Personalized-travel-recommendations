8123e4bbfd19cfe78a7e83f07623f3a8f7e2652a
jspark6815 <jspark6815@daum.net>
2025-06-03 12:59:36 +0900

    refactor: change to a scalable architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:personalized_travel_recommendations/screens/main_screen.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Travel Recommendations',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const MainScreen(),
-    );
-  }
+  runApp(const App());
 }
