330b7c1bafce87928ea19e33ad9eab136db45ee3
jspark6815 <jspark6815@daum.net>
2025-06-07 15:46:53 +0900

    refactor: update application namespace and ID to com.kbyjd.tripblock; remove old MainActivity file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kbyjd"
+    namespace = "com.kbyjd.tripblock"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kbyjd"
+        applicationId = "com.kbyjd.tripblock"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
