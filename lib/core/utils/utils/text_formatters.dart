1208a8c4c7a8fed2cec3a043d2712f98d54b3d9b
i-kye <grados0602@gmail.com>
2025-06-18 03:37:39 +0900

    refactor: Separate travel data and text formatters into dedicated files

---
+++ lib/core/utils/utils/text_formatters.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class CommaFormatter extends TextInputFormatter {
+  CommaFormatter();
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+
+    final numberStr = newValue.text.replaceAll(RegExp(r'[^0-9]'), '');
+    if (numberStr.isEmpty) {
+      return newValue.copyWith(text: '');
+    }
+
+    final int parsedInt = int.parse(numberStr);
+    final formatter = NumberFormat.currency(locale: 'ko', symbol: '');
+    String newText = formatter.format(parsedInt);
+
+    return newValue.copyWith(
+        text: newText,
+        selection: TextSelection.collapsed(offset: newText.length));
+  }
+}
+
+class PrefixInputFormatter extends TextInputFormatter {
+  final String prefix;
+
+  PrefixInputFormatter(this.prefix);
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (!newValue.text.startsWith(prefix)) {
+      final newText = prefix + newValue.text.replaceFirst(prefix, '');
+      return TextEditingValue(
+        text: newText,
+        selection: TextSelection.collapsed(offset: newText.length),
+      );
+    }
+
+    return newValue;
+  }
+}
